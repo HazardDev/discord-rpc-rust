0ee902d950b0c5d50f53cde7b99243e8fa4481ac
JewsOfHazard <logan.saso2016@gmail.com>
2018-03-07 21:33:04 -0800

    Bindgen helped, now I need it to leave

--- build.rs
+++ build.rs
@@ -4,15 +4,6 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // let bindings = bindgen::Builder::default()
-    //     .header(".\\discord-rpc\\include\\discord_rpc.h")
-    //     .generate()
-    //     .expect("Unable to generate bindings");
-
-    // let out_path = PathBuf::from("src");
-    // bindings
-    //     .write_to_file(out_path.join("bindings.rs"))
-    //     .expect("Couldn't write bindings!");
 
     println!(r"cargo:rustc-link-search=discord-rpc\lib\");
 }
